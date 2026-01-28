b72ad50bff7f941b3e2616e0cadbb7c447433edb
John Källén <johnkal@gmail.com>
2026-01-28 14:58:39 +0100

    Refactor: move IA I- and M-decoders into separate files

---
+++ src/Arch/IA64/IA64Disassembler.FDecoders.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reko.Arch.IA64;
+
+public partial class IA64Disassembler
+{
+    private static UnitDecoder MakeFdecoders()
+    {
+        var fp6bitExt0 = Sparse(27, 6, "  6 bit ext 0", Nyi("6 bit ext 0"),
+            (0, Instr(Mnemonic.break_f, F15)),
+            (1, Instr(Mnemonic.nop_f, F15)),
+            (0x10, Instr(Mnemonic.fmerge_s, F9)),
+            (0x11, Instr(Mnemonic.fmerge_ns, F9)),
+            (0x12, Instr(Mnemonic.fmerge_se, F9)),
+            (0x1A, Instr(Mnemonic.fcvt_fx_trunc, F10)),
+            (0x1B, Instr(Mnemonic.fcvt_fxu_trunc, F10)),
+            (0x1C, Instr(Mnemonic.fcvt_xf, F11)));
+        var fpReciprocalApprox0 = Mask(36, 1, "  Reciprocal approx 0",
+            Instr(Mnemonic.frcpa, F6),
+            Instr(Mnemonic.frsqrta, F7));
+        var fp6bitExt1 = Sparse(27, 6, "  6 bit ext 1", Nyi("6 bit ext 1"),
+            (0x10, Instr(Mnemonic.fpmerge_s, F9)));
+
+        var fpMisc0 = Mask(33, 1, "  FP Misc 0",
+            fp6bitExt0,
+            fpReciprocalApprox0);
+
+        var fpMisc1 = Mask(33, 1, "  FP Misc 1",
+            fp6bitExt1,
+            Nyi("Reciprocal approx 1")
+            );
+        var fpCompare = Nyi("FP Compare");
+        var fpClass = Nyi("FP Class");
+
+        var fma = Mask(36, 2, "  fma",
+            Instr(Mnemonic.fma, F1),
+            Instr(Mnemonic.fma_s, F1),
+            Instr(Mnemonic.fma_d, F1),
+            Instr(Mnemonic.fpma, F1));
+        var fms = Mask(36, 2, "  fms",
+            Instr(Mnemonic.fms, F1),
+            Instr(Mnemonic.fms_s, F1),
+            Instr(Mnemonic.fms_d, F1),
+            Instr(Mnemonic.fpms, F1));
+        var fnma = Mask(36, 2, "  fnma",
+            Instr(Mnemonic.fpma, F1),
+            Instr(Mnemonic.fnma_s, F1),
+            Instr(Mnemonic.fnma_d, F1),
+            Instr(Mnemonic.fpnma, F1));
+        var fselectXma = Mask(36, 1, "  fselext/xma",
+            Nyi("xma"),
+            Instr(Mnemonic.fselect, F3));
+
+        return new UnitDecoder('f', Mask(37, 4, "  F",
+            fpMisc0,
+            fpMisc1,
+            invalid,
+            invalid,
+
+            fpCompare,
+            fpClass,
+            invalid,
+            invalid,
+
+            fma,
+            fma,
+            fms,
+            fms,
+
+            fnma,
+            fnma,
+            fselectXma,
+            invalid));
+    }
+}
---
+++ src/Arch/IA64/IA64Disassembler.IDecoders.cs
@@ -0,0 +1,189 @@
+#region License
+/* 
+ * Copyright (C) 1999-2026 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using Reko.Core;
+using Reko.Core.Machine;
+
+namespace Reko.Arch.IA64;
+
+using WideDecoder = WideDecoder<IA64Disassembler, Mnemonic, IA64Instruction>;
+
+public partial class IA64Disassembler
+{
+
+    private static UnitDecoder MakeIdecoders(
+        WideDecoder compare,
+        WideDecoder aluMmAlu,
+        WideDecoder addImm22)
+    {
+        var misc6bitExt = Mask(31, 2, "  Misc I-Unit 6-bit Opcode Extensions",
+            Sparse(27, 4, "  0", Nyi("0"),
+                (0b0000, Instr(Mnemonic.break_i, InstrClass.Terminates, I32_1_20, qp)),
+                (0b0001, Instr(Mnemonic.nop_i, InstrClass.Linear | InstrClass.Padding, I32_1_20, qp)),
+                (0b1010, Instr(Mnemonic.mov_i, I27))),
+            Mask(27, 4, "  1",
+                Instr(Mnemonic.zxt1, I29),
+                Instr(Mnemonic.zxt2, I29),
+                Instr(Mnemonic.zxt4, I29),
+                invalid,
+
+                Instr(Mnemonic.sxt1, I29),
+                Instr(Mnemonic.sxt2, I29),
+                Instr(Mnemonic.sxt4, I29),
+                invalid,
+
+                Instr(Mnemonic.czx1_l, I29),
+                Instr(Mnemonic.czx2_l, I29),
+                invalid,
+                invalid,
+
+                Instr(Mnemonic.czx1_r, I29),
+                Instr(Mnemonic.czx2_r, I29),
+                invalid,
+                invalid),
+            Sparse(27, 4, "  2", invalid,
+                (0xA, Instr(Mnemonic.mov_i, I26))),
+            Sparse(27, 4, "  3", invalid,
+                (0x0, Instr(Mnemonic.mov, r1, ip)),
+                (0x1, Instr(Mnemonic.mov, I22)),
+                (0x2, Instr(Mnemonic.mov, I28)),
+                (0x3, Instr(Mnemonic.mov, r1, pr))));
+
+        var misc = Mask(33, 3, "  misc",
+            misc6bitExt,
+            Nyi("chk.s.i"),
+            Nyi("mov to pr.rot - imm44"),
+            Instr(Mnemonic.mov, pr, I23),
+
+            invalid,
+            invalid,
+            invalid,
+            Instr(Mnemonic.mov, I21));
+
+        var testbit = Mask(31, 1, "  y",
+            Mask(Bf((33, 1), (36, 1), (12, 1)), "  test",
+                Instr(Mnemonic.tbit_z, I16),
+                Instr(Mnemonic.tbit_z_unc, I16),
+                Instr(Mnemonic.tbit_z_and, I16),
+                Instr(Mnemonic.tbit_nz_and, I16),
+                Instr(Mnemonic.tbit_z_or, I16),
+                Instr(Mnemonic.tbit_nz_or, I16),
+                Instr(Mnemonic.tbit_z_or_andcm, I16),
+                Instr(Mnemonic.tbit_nz_or_andcm, I16)),
+            Mask(Bf((33, 1), (36, 1), (12, 1)), "  tnat",
+                Instr(Mnemonic.tnat_z, I17),
+                Instr(Mnemonic.tnat_z_unc, I17),
+                Instr(Mnemonic.tnat_z_and, I17),
+                Instr(Mnemonic.tnat_nz_and, I17),
+                Instr(Mnemonic.tnat_z_or, I17),
+                Instr(Mnemonic.tnat_nz_or, I17),
+                Instr(Mnemonic.tnat_z_or_andcm, I17),
+                Instr(Mnemonic.tnat_nz_or_andcm, I17)));
+
+        var deposit = Nyi("deposit");
+        var shiftTestBit = Mask(33, 1, "  shift/test bit",
+            Mask(34, 2, "  x_2",
+                testbit,
+                Mask(31, 1, "  y",
+                    Instr(Mnemonic.extr_u, I11),
+                    Instr(Mnemonic.extr, I11)),
+                invalid,
+                Nyi("shrp I10")),
+            Mask(34, 2, "  0",
+                testbit,
+                Mask(13, 1, "  y",
+                    Instr(Mnemonic.dep_z, I12),
+                    Instr(Mnemonic.dep_z, I13)),
+                invalid,
+                Nyi("dep - imm1 I14")));
+        var variableShift = Mask(32, 1, "  v_e",
+            Mask(34, 2, "  x_2a",
+                Sparse(28, 4, "x_2c:x_2b", invalid,
+                    (0, Instr(Mnemonic.shr_u, I5)),
+                    (2, Instr(Mnemonic.shr, I5)),
+                    (4, Instr(Mnemonic.shl, I7))),
+                invalid,
+                invalid,
+                invalid),
+            Nyi("x_2a == 1"));
+
+        var mmSize2 = Mask(32, 1, "  mm size 2",
+            Mask(34, 2, "  x_2a",
+                Sparse(28, 6, "  x_2a = 0", Nyi("x_2a = 0"),
+                    (0x00, Instr(Mnemonic.pshr4_u, I5))),
+                Nyi("x_2a = 1"),
+                Nyi("x_2a = 2"),
+                Nyi("x_2a = 3")
+            ),
+            Nyi("  v_e = 1"));
+
+        var mmSize4 = Mask(32, 1, "  mm size 4",
+            Mask(34, 2, "  x_2a",
+                Sparse(28, 2, "  x_2a = 0", Nyi("x_2a = 0"),
+                    (0, Mask(30, 2, "  x_2b = 0",
+                        Instr(Mnemonic.pshr4_u, I5),
+                        Instr(Mnemonic.pshl4, I7),
+                        invalid,
+                        invalid)),
+                    (2, Mask(30, 2, "  x_2b = 1",
+                        Instr(Mnemonic.pshr4, I5),
+                        invalid,
+                        invalid,
+                        invalid))),
+                Nyi("x_2a = 1"),
+                Sparse(28, 2, "  x_2a = 2", Nyi("x_2a = 2"),
+                    (2, Mask(30, 2, "  x_2b = 2",
+                        Nyi("pack4.sss I2"),
+                        Nyi("upack4.l I2"),
+                        Instr(Mnemonic.mix4_l, I2),
+                        invalid))),
+                Nyi("x_2a = 3")
+            ),
+            Nyi("  v_e = 1"));
+
+
+        var mmMpyShift = Mask(Bf((36, 1), (33, 1)), "  mm/mpy shift",
+            Nyi("  mm size 1"),
+            mmSize2,
+            mmSize4,
+            variableShift);
+
+        return new UnitDecoder('i', Mask(37, 4, "  I",
+            misc,
+            invalid,
+            invalid,
+            invalid,
+
+            deposit,
+            shiftTestBit,
+            invalid,
+            mmMpyShift,
+
+            aluMmAlu,
+            addImm22,
+            invalid,
+            invalid,
+
+            compare,
+            compare,
+            compare,
+            invalid));
+    }
+}
